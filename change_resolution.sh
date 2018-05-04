d29cb61ca70731a08150747a36f5b2bbbbb5c86f
TechUniv <38987728+TechUniv@users.noreply.github.com>
2018-05-04 10:22:14 -0700

    Initial commit of the change_resolution script

--- change_resolution.sh
+++ change_resolution.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Script to toggle between low/high resolution
-# JMD 5/2/2018
+# Script to toggle between low/high resolution particularly useful
+# for high resolution monitors where some applications load with tiny
+# icons.
+
+# To use, find your display name with this command: 
+# xrandr | grep "connected primary"
+# Output will look similar to this - in this example the display name is eDP-1:  
+# eDP-1 connected primary 3840x2160+0+0
+
+# Then replace the below resolutions with what you intend 
+# (eg 3840x2160 and 1920x1080)
+#
+# Tech U 5/2/2018
 
 res=`xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'`
 if [ "$res" == '3840x2160' ];then
